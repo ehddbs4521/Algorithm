3ea7a9e559ea488408a593b992369b8ada2330ed
bronze_yun_4521 <112162446+ehddbs4521@users.noreply.github.com>
2025-05-28 09:31:17 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.05 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -6,33 +6,34 @@ class Solution {
     static int[] dy = {1, -1, 0, 0};
     
     public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
+        int n=maps.length;
+        int m=maps[0].length;
         
-        Queue<int[]> que = new LinkedList<>();
-        boolean[][] visited = new boolean[n][m];
+        Queue<int[]> que=new LinkedList<>();
         
-        que.add(new int[]{0, 0, 1});
-        visited[0][0] = true;
+        boolean[][] visited=new boolean[n][m];
         
-        while (!que.isEmpty()) {
-            int[] now = que.poll();
-            int now_x = now[0];
-            int now_y = now[1];
-            int steps = now[2];
+        que.add(new int[]{0,0,1});
+        visited[0][0]=true;
+        
+        while(!que.isEmpty()){
+            int[] now=que.poll();
+            int now_x=now[0];
+            int now_y=now[1];
+            int now_cnt=now[2];
             
-            if (now_x == n - 1 && now_y == m - 1) {
-                return steps;
+            if(now_x==n-1 && now_y==m-1){
+                return now_cnt;
             }
             
-            for (int i = 0; i < 4; i++) {
-                int new_x = now_x + dx[i];
-                int new_y = now_y + dy[i];
+            for(int i=0;i<4;i++){
+                int new_x=now_x+dx[i];
+                int new_y=now_y+dy[i];
                 
-                if (new_x >= 0 && new_x < n && new_y >= 0 && new_y < m) {
-                    if (maps[new_x][new_y] == 1 && !visited[new_x][new_y]) {
-                        visited[new_x][new_y] = true;
-                        que.add(new int[]{new_x, new_y, steps + 1});
+                if(new_x>=0 && new_x<n && new_y>=0 && new_y<m){
+                    if(!visited[new_x][new_y] && maps[new_x][new_y]==1){
+                        que.add(new int[]{new_x,new_y,now_cnt+1});
+                        visited[new_x][new_y]=true;
                     }
                 }
             }
@@ -40,4 +41,5 @@ class Solution {
         
         return -1;
     }
+    
 }
