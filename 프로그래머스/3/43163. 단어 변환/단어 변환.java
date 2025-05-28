e140ff7eba72a3bc734fe2939c716f9334257d4f
bronze_yun_4521 <112162446+ehddbs4521@users.noreply.github.com>
2025-05-28 10:08:36 +0900

    [level 3] Title: 단어 변환, Time: 0.80 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,53 +1,60 @@
 import java.util.*;
+
 class Solution {
+    
     public int solution(String begin, String target, String[] words) {
+        
         return bfs(begin, target, words);
     }
     
     int bfs(String begin, String target, String[] words){
         Queue<Word> que=new LinkedList<>();
+        boolean visited[]=new boolean[words.length];
         que.add(new Word(begin,0));
-        boolean[] visited=new boolean[words.length];
-
+        
         while(!que.isEmpty()){
-            Word now=que.poll();
-            String now_word=now.word;
-            int now_cnt=now.cnt;
+            Word w=que.poll();
+            String now_word=w.word;
+            int now_cnt=w.cnt;
             
-            if(now_word.equals(target)){
+            if(target.equals(now_word)){
                 return now_cnt;
             }
             
             for(int i=0;i<words.length;i++){
-                if(!visited[i]&&diff(now_word,words[i])){
-                    visited[i]=true;
+                if(!visited[i] && diff(words[i],now_word)){
                     que.add(new Word(words[i],now_cnt+1));
+                    visited[i]=true;
                 }
             }
         }
         return 0;
     }
     
-    boolean diff(String word, String target){
-        int cnt=0;
+    boolean diff(String target,String word){
+        
+        int diff_cnt=0;
+        
         for(int i=0;i<target.length();i++){
-            if(word.charAt(i)!=target.charAt(i)){
-                cnt++;
+            if(target.charAt(i)!=word.charAt(i)){
+                diff_cnt++;
             }
         }
-        if(cnt==1){
-            return true;
+        
+        if(diff_cnt!=1){
+            return false;
         }
-        return false;
+        return true;
     }
     
-    class Word{
-        String word;
-        int cnt;
-        
-        Word(String word, int cnt){
-            this.word=word;
-            this.cnt=cnt;
-        }
+}
+
+class Word{
+    String word;
+    int cnt;
+    
+    Word(String word, int cnt){
+        this.word=word;
+        this.cnt=cnt;
     }
 }
