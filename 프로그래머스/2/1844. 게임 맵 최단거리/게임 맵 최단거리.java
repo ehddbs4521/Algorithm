62764a5620c081e8f874b8eaa7ee549e6543a94c
bronze_yun_4521 <112162446+ehddbs4521@users.noreply.github.com>
2025-05-29 20:37:03 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.01 ms, Memory: 56.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -6,39 +6,33 @@ class Solution {
     static int[] dy = {1, -1, 0, 0};
     
     public int solution(int[][] maps) {
-        int n=maps.length;
-        int m=maps[0].length;
-        
         Queue<int[]> que=new LinkedList<>();
-        
-        boolean[][] visited=new boolean[n][m];
-        
         que.add(new int[]{0,0,1});
+        
+        boolean[][] visited=new boolean[maps.length][maps[0].length];
         visited[0][0]=true;
         
         while(!que.isEmpty()){
             int[] now=que.poll();
-            int now_x=now[0];
-            int now_y=now[1];
-            int now_cnt=now[2];
+            int nowX=now[0];
+            int nowY=now[1];
+            int nowCount=now[2];
             
-            if(now_x==n-1 && now_y==m-1){
-                return now_cnt;
+            if(nowX==maps.length-1 && nowY==maps[0].length-1){
+                return nowCount;
             }
-            
             for(int i=0;i<4;i++){
-                int new_x=now_x+dx[i];
-                int new_y=now_y+dy[i];
+                int newX=nowX+dx[i];
+                int newY=nowY+dy[i];
                 
-                if(new_x>=0 && new_x<n && new_y>=0 && new_y<m){
-                    if(!visited[new_x][new_y] && maps[new_x][new_y]==1){
-                        que.add(new int[]{new_x,new_y,now_cnt+1});
-                        visited[new_x][new_y]=true;
+                if(newX>=0 && newX<maps.length && newY>=0 && newY<maps[0].length){
+                    if(!visited[newX][newY] && maps[newX][newY]==1){
+                        que.add(new int[]{newX,newY,nowCount+1});
+                        visited[newX][newY]=true;
                     }
                 }
             }
         }
-        
         return -1;
     }
     
