d06032ee11e94ba206d82c399360b5928b9e72d5
bronze_yun_4521 <112162446+ehddbs4521@users.noreply.github.com>
2025-07-04 09:41:57 +0900

    [level 3] Title: 합승 택시 요금, Time: 94.24 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,47 +1,35 @@
 import java.util.*;
+
 class Solution {
-   
-   public int solution(int n, int s, int a, int b, int[][] fares) {
-       int answer = 0;
-       int[][] costs=new int[n+1][n+1];
-       
-       for(int[] fare: fares){
-           costs[fare[0]][fare[1]]=fare[2];
-           costs[fare[1]][fare[0]]=fare[2];
-       }
-       int[][] distArray=new int[3][n+1];
-       for(int[] d : distArray){
-           Arrays.fill(d,Integer.MAX_VALUE);
-       }
-       
-       Queue<int[]> pq=new PriorityQueue<>((e1,e2)->e1[1]-e2[1]);
-       int[] start={s,a,b};
-       for(int j=0;j<3;j++){
-           int[] d=distArray[j];
-           pq.add(new int[]{start[j],0});
-           d[start[j]]=0;
-           while(!pq.isEmpty()){
-               int[] cur=pq.remove();
-               
-               for(int i=1;i<=n;i++){
-                   if(costs[cur[0]][i]==0) continue;
-                   if(d[i]>cur[1]+costs[cur[0]][i]){
-                       d[i]=cur[1]+costs[cur[0]][i];
-                       pq.add(new int[]{i,cur[1]+costs[cur[0]][i]});
-                   }
-               }
-           }
-       }
-       
-       int minCost=Integer.MAX_VALUE;
-       for(int i=1;i<=n;i++){
-           int sum=0;
-           for(int[] d:distArray){
-               sum+=d[i];
-           }
-           minCost=Math.min(minCost,sum);
-       }
-       
-       return minCost;
-   }
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        int answer = 0;
+        int[][] map = new int[n+1][n+1];
+        
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(map[i], 200 * 100_000);
+            map[i][i] = 0;
+        }
+        
+        for(int[] fare:fares){
+            map[fare[0]][fare[1]]=fare[2];
+            map[fare[1]][fare[0]]=fare[2];
+        }
+        
+        for(int k=1;k<=n;k++){
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=n;j++){
+                    if(map[i][j]>map[i][k]+map[k][j]){
+                        map[i][j]=map[i][k]+map[k][j];
+                    }
+                }
+            }
+        }
+        
+        answer=map[s][a]+map[s][b];
+        
+        for(int i=1;i<=n;i++){
+            answer=Math.min(answer,map[s][i]+map[i][a]+map[i][b]);
+        }
+        return answer;
+    }
 }
