f66fae485cbb5eca969ab9c176f59205fff3f93f
bronze_yun_4521 <112162446+ehddbs4521@users.noreply.github.com>
2025-09-27 22:34:23 +0900

    [level 2] Title: 타겟 넘버, Time: 7.85 ms, Memory: 99.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,20 +1,19 @@
 class Solution {
     int answer = 0;
-    
     public int solution(int[] numbers, int target) {
         backtrack(numbers, target, 0, 0);
         return answer;
     }
     
-    void backtrack(int[] numbers, int target, int index, int currentSum) {
-        if (index == numbers.length) {
-            if (currentSum == target) {
+    void backtrack(int[] numbers, int target, int now, int total){
+        if(now==numbers.length){
+            if(total==target){
                 answer++;
             }
             return;
         }
         
-        backtrack(numbers, target, index + 1, currentSum + numbers[index]);
-        backtrack(numbers, target, index + 1, currentSum - numbers[index]);
+        backtrack(numbers,target,now+1,total+numbers[now]);
+        backtrack(numbers,target,now+1,total-numbers[now]);
     }
 }
